a9263d59b3b12b4e7227fb4cb3cd8de74231a8a1
helloleemo <87022439+helloleemo@users.noreply.github.com>
2025-06-02 13:10:32 +0800

    build(init): init repo

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import MainSection from '@/components/MainSection.vue'
+</script>
+
+<template>
+  <MainSection msg="Welcome to Your Vue.js App"></MainSection>
+</template>
